27259e47713bd1b0478938f9f99375cff693b10f
Somen <Somensahoo@gmail.com>
2021-03-03 12:18:46 +0530

    new created

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join('__dirname', '../views'));
+
+
+app.get('/', (req, res) => {
+    // console.log("home page is opened");
+    res.render('home');
+})
+
+app.listen(3000, ()=>{
+    console.log("server 3000 connected");
+});
